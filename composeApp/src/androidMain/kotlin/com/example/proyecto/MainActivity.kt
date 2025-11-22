e8f5c0e1cc1e8c215e433a4dd1ebe24832becd10
Vollollo <cristobal.villalobos8@gmail.com>
2025-11-22 14:52:25 -0300

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/example/proyecto/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/proyecto/MainActivity.kt
@@ -1,25 +1,26 @@
 package com.example.proyecto
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.graphics.Color
+import com.example.proyecto.ui.FinanceApp
+import com.example.proyecto.viewmodel.FinanceViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        
         setContent {
-            App()
+            MaterialTheme {
+                Surface(color = Color.White) {
+                    val viewModel = FinanceViewModel(this@MainActivity)
+                    FinanceApp(viewModel)
+                }
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
